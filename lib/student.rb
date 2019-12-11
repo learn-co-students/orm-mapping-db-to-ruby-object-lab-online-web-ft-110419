28170691de69919d30e96b5cd16631e4e81d3edc
Nethelbert <gilblasse@gmail.com>
2019-12-11 19:31:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,47 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id,student.name,student.grade = row 
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql =  <<-SQL
+      SELECT * FROM students
+    SQL
+    
+    students = DB[:conn].execute(sql).map {|row| self.new_from_db(row) }
+    students
+  end
+  
+  def self.all_students_in_grade_9
+    self.all.select {|student| student.grade == "9" }
+  end
+  
+  def self.students_below_12th_grade
+    self.all.select {|student| student.grade.to_i <  12 }
+  end
+  
+  def self.first_X_students_in_grade_10(num)
+     self.all.select {|student| student.grade.to_i ==  10 }[0...num]
+  end
+  
+  def self.first_student_in_grade_10
+    self.all.find {|student| student.grade.to_i ==  10 }
+  end
+  
+  def self.all_students_in_grade_X(num)
+     self.all.select {|student| student.grade.to_i ==  num }
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+    row = DB[:conn].execute(sql,name)[0]
+    self.new_from_db(row)
   end
   
   def save
