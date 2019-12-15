7ed6a683473f7a9e52ade8e7ff3600e50418a655
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-15 20:30:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(arg)
-    self.students_below_12th_grade.select {|student| student.grade == 10}[0..arg]
+    self.students_below_12th_grade.select {|student| student.grade == 10}[0..arg-1]
   end
 
   def self.first_student_in_grade_10
